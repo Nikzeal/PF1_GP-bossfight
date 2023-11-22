83cc516cd2e4515440256d2e8e57b847a77f466c
Nikzeal <lacarn@usi.ch>
2023-11-22 15:21:42 +0100

    Add project structure and resources

---
+++ src/Untitled.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Untitled) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+(define BACKGROUND (empty-scene 1000 1000))
+(define sword (bitmap "sword.png"))
+
+(place-image sword 400 110 BACKGROUND)
